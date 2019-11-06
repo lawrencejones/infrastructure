5dbbe3a20e19f15da2b3b22f1ce4d6c1fc240496
Lawrence Jones <lawrjone@gmail.com>
2019-11-06 10:54:23 +0000

    vault/config/main.tf

---
+++ vault/config/main.tf
@@ -0,0 +1,50 @@
+################################################################################
+# Backend
+################################################################################
+
+terraform {
+  backend "gcs" {
+    bucket = "lawrjone-tfstate"
+    prefix = "terraform/state/vault/config"
+  }
+}
+
+################################################################################
+# Provider
+################################################################################
+
+# Authenticate against Vault by fetching the root token from GCS and decrypting
+# it every time we boot terraform. This means we need a Google login that has
+# permission to access the key in GCS, and decrypt it using KMS. As this is my
+# personal project, I'm running as me@lawrjone.xyz which is a superuser. For a
+# corporate deployment, this would be the terraform administrator.
+provider "vault" {
+  address = "https://vault.lawrjone.xyz"
+  token   = data.google_kms_secret.vault_token.plaintext
+}
+
+provider "http" {
+}
+
+data "google_storage_object_signed_url" "root_token_enc_url" {
+  bucket   = "lawrjone-vault"
+  path     = "root-token.enc"
+  duration = "1m"
+}
+
+data "http" "root_token_enc" {
+  url = data.google_storage_object_signed_url.root_token_enc_url.signed_url
+}
+
+data "google_kms_secret" "vault_token" {
+  crypto_key = "projects/lawrjone/locations/global/keyRings/vault/cryptoKeys/vault-init"
+  ciphertext = data.http.root_token_enc.body
+}
+
+################################################################################
+# Auth backends
+################################################################################
+
+resource "vault_auth_backend" "oidc" {
+  type = "oidc"
+}
